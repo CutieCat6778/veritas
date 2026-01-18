3f0dd9f7070e4ecd6142c0130c160074424d12c4
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2026-01-18 13:03:31 +0100

    [UPDATE]

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# Veritas 
+# Veritas
+
 It is a news-reading app for many people, who doesn't believe into the media anymore.
 It is built for double-checking the news.
--- ios/Veritas/Network.swift
+++ ios/Veritas/Network.swift
@@ -7,6 +7,6 @@ class Network {
     let apollo: ApolloClient
 
     init() {
-        apollo = ApolloClient(url: URL(string: "http://192.168.178.135:3000/query")!)
+        apollo = ApolloClient(url: URL(string: "https://veritas-server.thinis.de/query")!)
     }
 }
